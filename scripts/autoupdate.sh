96113439febc68af0f7f147f92b975f87b79885d
Rob Fuller <fullergalway@gmail.com>
2020-03-07 03:25:12 +0000

    latest

--- scripts/autoupdate.sh
+++ scripts/autoupdate.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -e
 date
 cd $(dirname $0)/..
-npm run build && git status | grep -q "nothing to commit" || git commit -a -m 'latest' && git push
+git pull
+npm run build
+git status | grep -q "nothing to commit" || git commit -a -m 'latest' && git push
