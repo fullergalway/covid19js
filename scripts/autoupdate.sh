eae750a53b81a1665368f8a40470847424f4e781
Rob Fuller <fullergalway@gmail.com>
2020-03-13 15:09:16 +0000

    latest

--- scripts/autoupdate.sh
+++ scripts/autoupdate.sh
@@ -5,9 +5,6 @@ cd $(dirname $0)/..
 #npm install
 npm run build
 git status | grep -q "nothing to commit" || {
-    head -1 dist/covid19.js > dist/covid19js.tmp
-    echo '\nif(typeof(module) !== "undefined") module.exports = covid19;' >> dist/covid19js.tmp
-    mv dist/covid19js.tmp dist/covid19.js
     now=$(date -Iseconds --utc | sed -e 's/+.*$/Z/')
     echo \"${now}\" > dist/updated.json
     git commit -a -m 'latest'
