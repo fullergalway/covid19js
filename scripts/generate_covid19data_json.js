a562887318499bcb4d34572ccbf372586ce6bccf
Rob Fuller <fullergalway@gmail.com>
2020-03-16 00:07:07 +0000

    latest

--- scripts/generate_covid19data_json.js
+++ scripts/generate_covid19data_json.js
@@ -3,6 +3,7 @@ const covid19data = require('../src/covid19data');
 const covid19data_json = __dirname + '/../dist/covid19data.json'
 const covid19 = require('../dist/covid19')
 const covid19_json = __dirname + '/../dist/covid19.json'
+const covid19latest_json = __dirname + '/../dist/covid19latest.json'
 const writejson =(filename, write)=>{
 fs.unlink(filename, ()=>{
         let out = fs.createWriteStream(filename);
@@ -11,9 +12,10 @@ fs.unlink(filename, ()=>{
       });
 }
 writejson(covid19data_json, (out)=>out.write(JSON.stringify(covid19data)));
-writejson(covid19_json, (out)=>{
-    let data = covid19.data();
+const writeabitpretty = (data,out)=> {
     out.write("[\n");
     out.write(data.map(l=>JSON.stringify(l)).join(",\n"));
-    out.write("\n];\n")
-})
+    out.write("\n]\n")
+}
+writejson(covid19_json, writeabitpretty.bind(null,covid19.data()))
+writejson(covid19latest_json, writeabitpretty.bind(null,covid19.data().latest()));
